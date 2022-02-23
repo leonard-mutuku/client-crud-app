1e92a58de8b943faeca8975b5417af71e8f9a266
leonard <lmutuku@techpitch.co.ke>
2022-02-23 18:11:54 +0300

    Multi Select Delete

--- src/main/java/com/client/controller/ClientsController.java
+++ src/main/java/com/client/controller/ClientsController.java
@@ -35,19 +35,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/clients")
 public class ClientsController {
-    
+
     private final ClientRepository clientRepository;
-    
+
     @Autowired
     private SessionController sessionController;
-    
+
     public ClientsController(ClientRepository clientRepository) {
         this.clientRepository = clientRepository;
     }
-    
+
     private Map fetchClientsData(Integer offset, Integer limit, String filter) {
         Sort sort = Sort.by("id").descending();
-        int size = (int) Math.ceil(offset /limit);
+        int size = (int) Math.ceil(offset / limit);
         Pageable pageable = PageRequest.of(size, limit, sort);
         Page page;
         if (filter == null || filter.equals("null")) {
@@ -61,7 +61,7 @@ public class ClientsController {
         response.put("pages", page.getTotalPages());
         return response;
     }
-    
+
     @GetMapping
     public ResponseEntity fetchClients(
             @RequestParam(defaultValue = "0") int offset,
@@ -72,20 +72,20 @@ public class ClientsController {
         Map response = fetchClientsData(offset, limit, filter);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
-    
+
     @GetMapping("/{id}")
     public Client getClient(@PathVariable Long id) {
         sessionController.getLoggedInUser(false);
         return clientRepository.findById(id).orElseThrow(RuntimeException::new);
     }
-    
+
     @PostMapping
     public ResponseEntity createClient(@RequestBody Client client) throws URISyntaxException {
         sessionController.getLoggedInUser(false);
         Client savedClient = clientRepository.save(client);
         return ResponseEntity.created(new URI("/clients/" + savedClient.getId())).body(savedClient);
     }
-    
+
     @PutMapping("/{id}")
     public ResponseEntity updateClient(@PathVariable Long id, @RequestBody Client client) {
         sessionController.getLoggedInUser(false);
@@ -95,22 +95,24 @@ public class ClientsController {
         currentClient.setEmail(client.getEmail());
         currentClient.setPhoneNumber(client.getPhoneNumber());
         currentClient = clientRepository.save(client);
-        
+
         return ResponseEntity.ok(currentClient);
     }
-    
-    @DeleteMapping("/{id}")
+
+    @DeleteMapping("/{ids}")
     public ResponseEntity deleteClient(
-            @PathVariable Long id,
+            @PathVariable Long[] ids,
             @RequestParam int offset,
             @RequestParam int limit,
             @RequestParam(required = false) String filter) {
         sessionController.getLoggedInUser(false);
-        clientRepository.deleteById(id);
-        int items = 1;
+        for (Long id : ids) {
+            clientRepository.deleteById(id);
+        }
+        int items = ids.length;
         int start = (offset + limit) - items;
         Map response = fetchClientsData((start), items, filter);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
-    
+
 }
